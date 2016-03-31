7ce9506d857e3e0949b3b24f70f15f35b8980324
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-04-01 02:32:08 +0530

    Dashboard activity created

---
+++ app/src/main/java/vishwajeetv/com/learnapp/DashboardActivity.java
@@ -0,0 +1,14 @@
+package vishwajeetv.com.learnapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DashboardActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+}
