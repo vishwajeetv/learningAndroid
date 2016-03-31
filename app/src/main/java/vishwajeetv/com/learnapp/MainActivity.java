e35b88629681b94621fd11f8fdabcc8fe85af7f3
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-04-01 02:35:19 +0530

    Now creating DB at launch!

--- app/src/main/java/vishwajeetv/com/learnapp/MainActivity.java
+++ app/src/main/java/vishwajeetv/com/learnapp/MainActivity.java
@@ -1,12 +1,18 @@
 package vishwajeetv.com.learnapp;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +20,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Gets the data repository in write mode
+        DatabaseHelper mDbHelper = new DatabaseHelper(this);
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(DatabaseContract.User.PRIMARY_KEY, 1);
+        values.put(DatabaseContract.User.COLUMN_EMAIL, "vishwajeetvatharkar@gmail.com");
+        values.put(DatabaseContract.User.COLUMN_NAME, "Vishwajeet");
+        values.put(DatabaseContract.User.COLUMN_PASSWORD, "qwerty");
+
+        long newRowId;
+        newRowId = db.insert(
+                DatabaseContract.User.TABLE_NAME,
+                DatabaseContract.User.COLUMN_NAME,
+                values);
 
     }
 
@@ -21,5 +42,17 @@ public class MainActivity extends Activity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
         return true;
+    }
+
+    public void login(View view)
+    {
+        Intent intent = new Intent(this, DashboardActivity.class);
+        Context context = getApplicationContext();
+        CharSequence text = "Login Successful!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+        startActivity(intent);
     }
 }
