26ab9d7e9c444b9085286d3855ba0d48bcbca5a7
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-04-01 03:53:16 +0530

    menu moved to dashboard

--- app/src/main/java/vishwajeetv/com/learnapp/DashboardActivity.java
+++ app/src/main/java/vishwajeetv/com/learnapp/DashboardActivity.java
@@ -1,7 +1,11 @@
 package vishwajeetv.com.learnapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.TextView;
 
 public class DashboardActivity extends Activity {
 
@@ -9,6 +13,18 @@ public class DashboardActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Intent intent = getIntent();
+        String nameUser = intent.getStringExtra("USER_NAME");
+        TextView welcomeMessage = (TextView)findViewById(R.id.welcomeMessage);
+        welcomeMessage.setText(nameUser);
+
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
 
 }
