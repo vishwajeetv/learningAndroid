a07c2e3824b2b9a15532e6e798b03acf9e59c4a7
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2016-04-01 03:54:04 +0530

    SQLite Querying done!

--- app/src/main/java/vishwajeetv/com/learnapp/MainActivity.java
+++ app/src/main/java/vishwajeetv/com/learnapp/MainActivity.java
@@ -4,55 +4,106 @@ import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.Serializable;
+
 public class MainActivity extends Activity {
 
+    DatabaseHelper mDbHelper = new DatabaseHelper(this);
+
+    SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Gets the data repository in write mode
-        DatabaseHelper mDbHelper = new DatabaseHelper(this);
-        SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
-        ContentValues values = new ContentValues();
-        values.put(DatabaseContract.User.PRIMARY_KEY, 1);
-        values.put(DatabaseContract.User.COLUMN_EMAIL, "vishwajeetvatharkar@gmail.com");
-        values.put(DatabaseContract.User.COLUMN_NAME, "Vishwajeet");
-        values.put(DatabaseContract.User.COLUMN_PASSWORD, "qwerty");
-
-        long newRowId;
-        newRowId = db.insert(
-                DatabaseContract.User.TABLE_NAME,
-                DatabaseContract.User.COLUMN_NAME,
-                values);
+//        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+//
+//        ContentValues values = new ContentValues();
+//        values.put(DatabaseContract.User.PRIMARY_KEY, 1);
+//        values.put(DatabaseContract.User.COLUMN_EMAIL, "vishwajeetvatharkar@gmail.com");
+//        values.put(DatabaseContract.User.COLUMN_NAME, "Vishwajeet");
+//        values.put(DatabaseContract.User.COLUMN_PASSWORD, "qwerty");
+//        Log.e("values",values.toString());
+//        long newRowId;
+//        newRowId = db.insert(
+//                DatabaseContract.User.TABLE_NAME,
+//                DatabaseContract.User.COLUMN_NAME,
+//                values);
 
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return true;
-    }
 
     public void login(View view)
     {
-        Intent intent = new Intent(this, DashboardActivity.class);
-        Context context = getApplicationContext();
-        CharSequence text = "Login Successful!";
+        EditText emailText   = (EditText)findViewById(R.id.email);
+        EditText passwordText   = (EditText)findViewById(R.id.password);
+
+        String email = emailText.getText().toString();
+        String password = passwordText.getText().toString();
+        Log.e("email",email);
         int duration = Toast.LENGTH_SHORT;
 
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-        startActivity(intent);
+
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+//
+        String[] projection = {
+                DatabaseContract.User.PRIMARY_KEY,
+                DatabaseContract.User.COLUMN_NAME
+        };
+//
+        String selection =
+           DatabaseContract.User.COLUMN_EMAIL + " LIKE ? AND "  + DatabaseContract.User.COLUMN_PASSWORD + " LIKE ? ";
+
+//
+        String selectionArgs[] = {
+                email , password
+        };
+
+        Cursor cursor = db.query(
+                DatabaseContract.User.TABLE_NAME,  // The table to query
+                projection,                               // The columns to return
+                selection,                                // The columns for the WHERE clause
+                selectionArgs,                        // The values for the WHERE claus
+                null,null,null
+        );
+//
+       if(cursor.moveToFirst())
+       {
+           String nameUser = cursor.getString(1);
+           Log.e("user name",nameUser);
+
+           Context context = getApplicationContext();
+           CharSequence text = "Login Successful!";
+           Toast toast = Toast.makeText(context, text, duration);
+           toast.show();
+
+           Intent intent = new Intent(this, DashboardActivity.class);
+           intent.putExtra("USER_NAME",nameUser);
+           startActivity(intent);
+       }
+        else
+       {
+           Context context = getApplicationContext();
+           CharSequence text = "Invalid Email / Password";
+
+           Toast toast = Toast.makeText(context, text, duration);
+           toast.show();
+
+       }
+
     }
 }
